696ecc987858ccecbc22e8f85bff43784c22c512
sam7072 <zarathos212@gmail.com>
2020-01-22 09:41:12 -0500

    serie_2

---
+++ serie/src/serie/series.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serie;
+
+/**
+ *
+ * @author Usuario
+ */
+public class series {
+    
+    int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public series(int num) {
+        this.num = num;
+    }
+
+    
+    
+    
+}
