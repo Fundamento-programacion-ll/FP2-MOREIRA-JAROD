b20bfa48e8d146109316ec1c0ca2491e57d233ea
sam7072 <zarathos212@gmail.com>
2020-02-10 09:46:00 -0500

    javaScript y funciones

---
+++ 01-javaScript.js
@@ -0,0 +1,32 @@
+var json1 = {
+nombre: "juan",
+edad: 24,
+casado: "no",
+mascota : ['pepe'],
+
+colegio: {
+    nombrecolegio : 'don bosco',
+    telefono : 098891463,
+a:'',
+}
+
+};
+var jsonSam={
+    nombre:"Sam",
+    apellido:"moreira",
+    edad:21,
+}
+
+console.log("hola");
+console.log(json1);
+console.log(json.colegio.colegio.nombrecolegio);
+var nu = null;
+console.log("tipo:"+typeof nu);
+var numero=1;
+console.log(json1+jsonSam+numero);
+var bool=true;
+console.log(json1+jsonSam+bool+numero);
+console.log(underfined+numero);
+
+
+
---
+++ funciones.js
@@ -0,0 +1,14 @@
+var miFuncion  = function(n1,n2){
+    console.log('funcion 1');
+    return n1+n2;
+
+};
+//funcion de flecha gorda
+var miFuncion2 = (n1,n2)=>{
+//return'khe';
+return n1+n2;
+
+};
+miFuncion();
+console.log(miFuncion(1,4));
+console.log(miFuncion2());
